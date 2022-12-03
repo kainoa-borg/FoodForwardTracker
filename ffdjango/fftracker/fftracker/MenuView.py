501b690c6921686632457c24e8136a56cf0991fc
A. McNeese <angela.mcneese@ucdenver.edu>
2022-12-03 01:43:49 -0700

    SQL fix

--- ffdjango/fftracker/fftracker/MenuView.py
+++ ffdjango/fftracker/fftracker/MenuView.py
@@ -10,20 +10,22 @@ from .models import MealPlans, Recipes
 class MenuSerializer(ModelSerializer):
 	r_num = serializers.IntegerField()
 	r_name = serializers.CharField(max_length=200)
+	meal_name = serializers.CharField(max_length=200)
+	snack_name = serializers.CharField(max_length=200)
 	class Meta():
 		model = MealPlans
-		fields = ('m_id', 'm_date', 'snack_r_num', 'meal_r_num', 'num_servings', 'r_num', 'r_name')
+		fields = ('m_id', 'm_date', 'snack_r_num', 'meal_r_num', 'num_servings', 'r_num', 'r_name', 'meal_name', 'snack_name')
 
 class MenuView(viewsets.ViewSet):
 	def list(self, request):
-		keys = ('m_id', 'm_date', 'snack_r_num', 'meal_r_num', 'num_servings', 'r_num', 'r_name')
-		query = "SELECT mp.*, (SELECT r_name FROM recipes WHERE (mp.meal_r_num = r_num) OR (mp.snack_r_num = r_num)) AS r_name FROM meal_plans mp"
+		keys = ('m_id', 'm_date', 'snack_r_num', 'meal_r_num', 'num_servings', 'r_num', 'r_name', 'meal_name', 'snack_name')
+		query = "SELECT mp.*, (SELECT r_name FROM recipes WHERE (mp.meal_r_num = r_num) AS meal_name, (SELECT r_name FROM recipes WHERE (mp.snack_r_num = r_num)) AS snack_name FROM meal_plans mp"
 		queryset = execute_query(query, keys, many=True)
 		serializer = MenuSerializer(queryset, many=True)
 		return Response(serializer.data)
 	def retrieve(self, request, pk):
-		query = "SELECT mp.*, (SELECT r_name FROM recipes WHERE (mp.meal_r_num = r_num) OR (mp.snack_r_num = r_num)) AS r_name FROM meal_plans mp"%(pk)
-		keys = ('m_id', 'm_date', 'snack_r_num', 'meal_r_num', 'num_servings', 'r_num', 'r_name')
+		query = "SELECT mp.*, (SELECT r_name FROM recipes WHERE (mp.meal_r_num = r_num) AS meal_name, (SELECT r_name FROM recipes WHERE (mp.snack_r_num = r_num)) AS snack_name FROM meal_plans mp"%(pk)
+		keys = ('m_id', 'm_date', 'snack_r_num', 'meal_r_num', 'num_servings', 'r_num', 'r_name', 'meal_name', 'snack_name')
 		queryset = execute_query(query, keys)
 		serializer = MenuSerializer(queryset)
 		return Response(serializer.data)
