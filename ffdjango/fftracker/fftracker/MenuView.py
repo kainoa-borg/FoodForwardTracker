d84c790581a7af4406e70cb05c48521dda725359
A. McNeese <angela.mcneese@ucdenver.edu>
2022-12-03 02:10:09 -0700

    all charfield

--- ffdjango/fftracker/fftracker/MenuView.py
+++ ffdjango/fftracker/fftracker/MenuView.py
@@ -8,7 +8,7 @@ from rest_framework import status
 from .models import MealPlans, Recipes
 
 class MenuSerializer(ModelSerializer):
-	r_num = serializers.IntegerField()
+	r_num = serializers.CharField(max_length=200)
 	r_name = serializers.CharField(max_length=200)
 	meal_name = serializers.CharField(max_length=200)
 	snack_name = serializers.CharField(max_length=200)
