38922f9d24db845d6a1023a32ef4ee793898b8b0
A. McNeese <angela.mcneese@ucdenver.edu>
2022-12-03 00:39:39 -0700

    manu view

---
+++ ffdjango/fftracker/fftracker/MenuView.py
@@ -0,0 +1,49 @@
+from .helperfuncs import execute_query
+from rest_framework.response import Response
+from rest_framework import viewsets
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from rest_framework import status
+from .models import MealPlans, Recipes
+
+class RecipeSerializer(serializers.ModelSerializer):
+	class Meta():
+		model = Recipes
+		fields = ('__all__')
+
+class MealPlansSerializer(serializers.ModelSerializer):
+	class Meta():
+		model = MealPlans
+		fields = ('__all__')
+
+class MealPlansView(viewsets.ModelViewSet):
+	queryset = MealPlans.objects.all()
+	serializer_class = MealPlansSerializer
+
+	#class Meta():
+	#	model = MealPlans
+	#	fields = ('m_id', 'm_date', 'snack_r_num', 'meal_r_num', 'num_servings')
+
+class MenuRecipeSerializer(serializers.ModelSerializer):
+	r_data = RecipeSerializer(many=True)
+	class Meta():
+		model = MealPlans
+		depth = 1
+		fields = ('m_id', 'm_date', 'snack_r_num', 'meal_r_num', 'num_servings', 'r_data')
+	def create(self, validated_data):
+		recipe_data = validated_data.pop('r_data')
+		mp_model = Recipes.objects.create(**validated_data)
+		for data in recipe_data: 
+			data['meal_r_num'] = mp_model
+			data['m_id'] = Recipes.objects.latest('m_id').m_id + 1
+			data_model = Recipes.objects.create(**data)
+		return mp_model
+
+		#query = "SELECT mp.m_date, r.r_name FROM meal_plans mp INNER JOIN recipes r ON mp.meal_r_num = r.r_num OR mp.snack_r_num = r.r_num"%(pk)
+		#keys = ('mp.m_date', 'r.r_name')
+
+class MenuView(viewsets.ModelViewSet):
+	queryset = MealPlans.objects.all()
+	serializer_class = MenuRecipeSerializer
+
