2720b0f2447843267cc27f46e497cdd4f290039c
A. McNeese <angela.mcneese@ucdenver.edu>
2022-12-07 22:16:19 -0700

    packaging list front and backend

--- ffreact-prod/src/Packaging/PackagingRow.js
+++ ffreact-prod/src/Packaging/PackagingRow.js
@@ -4,26 +4,25 @@ import React from 'react'
 // Takes: key of current row, the state of the Packaging Page's Packaging list, deletePackaging callback, handleEditClick callback
 // Returns a Packaging table row component 
 const PackagingRow = (props) => {
-    const {thisKey, packaging, deletePackaging, handleEditClick } = props;
+    const {thisKey, package_item, deletePackaging, handleEditClick } = props;
     const key = thisKey;
-    const pac = packaging;
+    const pac = package_item;
 
     // HTML structure of this component
     return (
         <tr key={key}>
             <td>{pac.p_id}</td>
             <td>{pac.package_type}</td>
-            <td>{String(pac.unit_qty)}</td>
+            <td>{pac.unit_qty}</td>
             <td>{pac.unit_cost}</td>
-            <td>{String(pac.qty_holds)}</td>
+            <td>{pac.qty_holds}</td>
             <td>{pac.unit}</td>
-            <td>placeholder</td>
-            <td>{String(pac.returnable)}</td>
+            <td>{pac.returnable}</td>
             <td>{String(pac.in_date)}</td>
-            <td>{String(pac.in_qty)}</td>
+            <td>{pac.in_qty}</td>
             <td>{String(pac.exp_date)}</td>
-            <td>{String(pac.qty_on_hand)}</td>
-            <td>{String(pac.flat_fee)}</td>
+            <td>{pac.qty_on_hand}</td>
+            <td>{pac.flat_fee}</td>
             <td>{pac.psupplier_name}</td>
             <td>{pac.pref_psupplier_name}</td>
             {/* When edit is clicked, handleEditClick is called with this row's key */}
