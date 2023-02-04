1805b4e9c174c02ed17ef1b71dd003950e98cc67
kaiborg <kainoa.accs@gmail.com>
2023-02-04 16:05:42 -0700

    Formatted landing page to match mockup

--- ffreact-prod/src/LandingPage.js
+++ ffreact-prod/src/LandingPage.js
@@ -1,33 +1,80 @@
 import React from 'react'
 import { useState } from 'react'
 import { Fragment } from 'react'
-import { Container, Button, Typography } from '@mui/material'
+import { Container, Button, Typography, Card, Grid, CardContent, CardActionArea } from '@mui/material'
 
 
 const LandingPage = (props) => {
     const handlePageClick = props.handlePageClick;
     // HTML structure of this component
     return (
-        <header  className="App-header">
-        <Fragment>
-            <h2>Landing Page</h2>
-            <Button variant='contained' onClick={() => handlePageClick('households')}>
-                Clients
-            </Button>
-            <Button variant='contained' onClick={() => handlePageClick('mealsPage')}>
-                Meals
-            </Button>
-            <Button variant='contained' onClick={() => handlePageClick('inventoryPage')}>
-                Inventory
-            </Button>
-            <Button variant='contained' onClick={() => handlePageClick('reports')}>
-                Reports
-            </Button>
-            <Button variant='contained' onClick={() => handlePageClick('userList')}>
-                Administration
-            </Button>
-        </Fragment>
-        </header>
+        <Grid 
+            container 
+            justifyContent='space-around' 
+            alignItems='stretch'
+            direction='row'
+            spacing={2}
+            sx={{minHeight: '60vh', margin: 'auto'}}>
+            <Grid item sm={12} md={4}>
+                <Card elevation='4' sx={{height: '100%'}}>
+                    <CardActionArea onClick={() => handlePageClick('households')}>
+                        <CardContent>
+                            <Typography component='div' variant='h5'>Clients</Typography>
+                            <Typography component='div' variant='body1'>- View client information</Typography>
+                            <Typography component='div' variant='body1'>- Add/Update client information</Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>    
+            </Grid>
+
+            <Grid item sm={12} md={4}>
+                <Card elevation='4' sx={{height: '100%'}}>
+                    <CardActionArea onClick={() => handlePageClick('mealsPage')}>
+                        <CardContent>
+                            <Typography component='div' variant='h5'>Meals</Typography>
+                            <Typography component='div' variant='body1'>- View meals</Typography>
+                            <Typography component='div' variant='body1'>- Add/Update meals</Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>    
+            </Grid>
+
+            <Grid item sm={12} md={4}>
+                <Card elevation='4' sx={{height: '100%'}}>
+                    <CardActionArea onClick={() => handlePageClick('inventoryPages')}>
+                        <CardContent>
+                            <Typography component='div' variant='h5'>Inventory</Typography>
+                            <Typography component='div' variant='body1'>- View ingredients and packaging</Typography>
+                            <Typography component='div' variant='body1'>- Add/Update ingredients and packaging</Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>    
+            </Grid>
+
+            <Grid item sm={12} md={4}>
+                <Card elevation='4' sx={{height: '100%'}}>
+                    <CardActionArea onClick={() => handlePageClick('reports')}>
+                        <CardContent>
+                            <Typography component='div' variant='h5'>Reports</Typography>
+                            <Typography component='div' variant='body1'>- View reports</Typography>
+                            <Typography component='div' variant='body1'>- Print or send reports via email</Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>    
+            </Grid>
+
+            <Grid item sm={12} md={4}>
+                <Card elevation='4' sx={{height: '100%'}}>
+                    <CardActionArea onClick={() => handlePageClick('reports')}>
+                        <CardContent>
+                            <Typography component='div' variant='h5'>Administration</Typography>
+                            <Typography component='div' variant='body1'>- View users in the system</Typography>
+                            <Typography component='div' variant='body1'>- Add/Edit users</Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>    
+            </Grid>
+        </Grid>
     );
 }
 
