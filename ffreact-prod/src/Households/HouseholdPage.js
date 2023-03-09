55ab27666d31b5882683062f30711aba5dc88ad6
A. McNeese <angela.mcneese@ucdenver.edu>
2023-03-08 22:27:19 -0700

    Clients headers updated and fixed

--- ffreact-prod/src/Households/HouseholdPage.js
+++ ffreact-prod/src/Households/HouseholdPage.js
@@ -32,12 +32,12 @@ export default function HouseholdPage() {
         { field: 'num_child_gt_6', headerName: '7-17',  type: 'number', width: 70, editable: true },
         { field: 'num_child_lt_6', headerName: '0-6', type: 'number', width: 70, editable: true },
         { field: 'phone', headerName: 'Phone', width: 110, type: 'phone', editable: true },
+        { field: 'sms_flag', headerName: 'SMS', width: 70, type: 'boolean', editable: true, valueParser: (value) => value ? 1 : 0 },
         { field: 'street', headerName: 'Street', width: 160, type: 'string', editable: true },
         { field: 'city', headerName: 'City', width: 100, type: 'string', editable: true },
         { field: 'state', headerName: 'State', width: 70, type: 'string', editable: true },
         { field: 'pcode', headerName: 'Zip Code', width: 80, type: 'string', editable: true, /*valueFormatter: (value) => {return value}*/ },
         { field: 'delivery_notes', headerName: 'Delivery Notes', width: 100, editable: true },
-        { field: 'sms_flag', headerName: 'SMS', width: 70, type: 'boolean', editable: true, valueParser: (value) => value ? 1 : 0 },
         { field: 'veg_flag', headerName: 'Veg', width: 70, type: 'boolean', editable: true, valueParser: (value) => value ? 1 : 0 },
         { field: 'gf_flag', headerName: 'Gluten Free', width: 70, type: 'boolean', editable: true, valueParser: (value) => value ? 1 : 0},
         { field: 'hh_allergies', headerName: 'Allergies', width: 100, type: 'string', editable: true, 
@@ -57,6 +57,7 @@ export default function HouseholdPage() {
             children: [{ field: 'num_adult' }, { field: 'num_child_gt_6' }, { field: 'num_child_lt_6' }],
         },
         {
+            groupId: 'contact_details',
             headerName: 'Contact Details',
             children: [{field: 'phone' }, { field: 'sms_flag'}]
         },
@@ -67,6 +68,7 @@ export default function HouseholdPage() {
         },
         { field: 'delivery_notes'},
         {
+            groupId: 'diet',
             headerName: 'Dietary Requirements',
             children:[{ field: 'veg_flag' }, { field: 'gf_flag'}, { field: 'hh_allergies'}]
         },
