1123e026e8c8ae6a74e4b4253ef56b17223ff94a
kainoa-borg <48833895+kainoa-borg@users.noreply.github.com>
2023-03-15 11:46:43 -0600

    Ingredient and Packaging supplier serializer fix

--- ffdjango/fftracker/fftracker/IngredientViews.py
+++ ffdjango/fftracker/fftracker/IngredientViews.py
@@ -26,6 +26,8 @@ class IngredientUsageSerializer(ModelSerializer):
 class IngredientInvSerializer(ModelSerializer):
 	isupplier = SupplierSerializer(read_only=True)
 	pref_isupplier = SupplierSerializer(read_only=True)
+	isupplier_id = serializers.IntegerField(allow_null=True)
+	pref_isupplier_id = serializers.IntegerField(allow_null=True)
 	ingredient_usage = IngredientUsageSerializer(required=False, allow_null=True, many=True)
 	class Meta():
 		model = Ingredients
