a1c338b8442545405cc782ee28f11f8be675a4d5
A. McNeese <angela.mcneese@ucdenver.edu>
2023-03-27 20:05:33 -0600

    initial cost/ing. purchase reports

--- ffdjango/fftracker/fftracker/IngPurchaseViews.py
+++ ffdjango/fftracker/fftracker/IngPurchaseViews.py
@@ -14,7 +14,7 @@ class IngredientUsageSerializer(ModelSerializer):
 		model = IngredientUsages
 		fields = ('used_date', 'used_qty')
 
-class IngredientInvSerializer(ModelSerializer):
+class IPLSerializer(ModelSerializer):
 	isupplier = serializers.CharField(max_length=200)
 	pref_isupplier = serializers.CharField(max_length=200)
 	ingredient_usage = IngredientUsageSerializer()
@@ -23,38 +23,26 @@ class IngredientInvSerializer(ModelSerializer):
 		fields = ('i_id', 'ingredient_name', 'pkg_type', 'storage_type', 'in_date', 'in_qty', 'ingredient_usage', 'unit', 'exp_date', 'unit_cost', 'flat_fee', 'isupplier', 'pref_isupplier')
 
 # Create your views here.
-class IngredientInvView(viewsets.ViewSet):
+class IPLView(viewsets.ViewSet):
 	def list(self, request):
 		#keys = ('i_id', 'ingredient_name', 'pkg_type', 'storage_type', 'in_date', 'in_qty', 'unit', 'exp_date', 'qty_on_hand', 'unit_cost', 'flat_fee', 'isupplier_id', 'pref_supplier_id', 'isupplier_name', 'pref_isupplier_name')
 		#query = "SELECT i.*, (SELECT s_name FROM supplier WHERE i.isupplier_id = s_id) AS isupplier_name, (SELECT s_name FROM supplier WHERE i.pref_isupplier_id = s_id) AS pref_supplier_name FROM ingredients i"
 		#queryset = execute_query(query, keys, many=True)
 		queryset = Ingredients.objects.prefetch_related(Prefetch('ingredient_usage', queryset=IngredientUsages.objects.all())).all().all()
 		print(queryset[0].__dict__['_prefetched_objects_cache']['ingredient_usage'].__dict__)
-		serializer = IngredientInvSerializer(queryset, many=True)
+		serializer = IPLSerializer(queryset, many=True)
 		return Response(serializer.data)
 	def retrieve(self, request, pk):
 		query = "SELECT i.*, (SELECT s_name FROM supplier WHERE i.isupplier_id = s_id) AS isupplier_name, (SELECT s_name FROM supplier WHERE i.pref_isupplier_id = s_id) AS pref_supplier_name FROM ingredients i WHERE i.i_id=%s"%(pk)
 		keys = ('i_id', 'ingredient_name', 'pkg_type', 'storage_type', 'in_date', 'in_qty', 'unit', 'exp_date', 'qty_on_hand', 'unit_cost', 'flat_fee', 'isupplier_id', 'pref_supplier_id', 'isupplier_name', 'pref_isupplier_name')
 		queryset = execute_query(query, keys)
-		serializer = IngredientInvSerializer(queryset)
+		serializer = IPLSerializer(queryset)
 		return Response(serializer.data)
 	def update(self, request, pk):
 		update_obj = Ingredients.Objects.get(pk)
-		serializer = IngredientInvSerializer(update_obj, data=request.data)
+		serializer = IPLSerializer(update_obj, data=request.data)
 		if serializer.is_valid():
 			obj = serializer.save()
 			obj.save()
 			return Response(status=status.HTTP_200_OK)
 		return Response(status=status.HTTP_200_BADREQUEST)
-	def create(self, request):
-		data = request.data
-		serializer = IngredientInvSerializer(data=data)
-		if serializer.is_valid():
-			obj = serializer.save()
-			obj.save()
-			return Response(obj, status=200)
-		return Response(status=400)
-	def destroy(self, request, pk):
-		ingredient = Ingredients.Objects.get(pk)
-		ingredient.delete()
-		return Response(status=204)
--- ffreact-prod/src/Reports/CostTotals.js
+++ ffreact-prod/src/Reports/CostTotals.js
@@ -1,24 +1,35 @@
 import React, { useState, useEffect} from 'react'
 import axios from 'axios'
 import ReusableTable from '../ReusableTable.js'
+import { DataGrid, GridToolbarContainer, GridToolbarExport } from '@mui/x-data-grid';
+import { Box, Button, Input, InputLabel, Snackbar, Typography, Stack, FormControl} from '@mui/material';
 
 // Packaging List Component
-export default function CostTotals() {
-    const [packaging, setPackaging] = useState(undefined);
-    // const [ingredients, setIngredients] = useState(undefined);
+export default function MealPlanReport() {
+    const [mealPlans, setMealPlans] = useState([]);
+    const [dateRange, setDateRange] = useState([]);
+    const [searchingSBOpen, setSearchingSBOpen] = useState(false);
+    const [resultsFoundSBOpen, setResultsFoundSBOpen] = useState(false);
+    const [noResultsSBOpen, setNoResultsSBOpen] = useState(false);
+    const [ingredients, setIngredients] = useState(undefined);
 
-    useEffect(() => {
-        getDBPackaging();
-        // getDBIngredients();
-    }, []);
+    const currencyFormatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
 
-    const getDBPackaging = () => {
+    const getDBMealPlanReport = (dateRange) => {
+        setSearchingSBOpen(true);
         axios({
             method: "GET",
-            url:"http://4.236.185.213:8000/api/packaging-reports"
+            url:"http://4.236.185.213:8000/api/mealplanreport/",
+            params: dateRange
           }).then((response)=>{
-            const pkgRetData = response.data
-            setPackaging(pkgRetData);
+            // console.log(response.data);
+            if (response.data.length > 0) setResultsFoundSBOpen(true);
+            else setNoResultsSBOpen(true);
+
+            setMealPlans(response.data);
           }).catch((error) => {
             if (error.response) {
               console.log(error.response);
@@ -28,7 +39,7 @@ export default function CostTotals() {
           });
     }
 
-   /* const getDBIngredients = () => {
+    const getDBIngredients = () => {
         axios({
             method: "GET",
             url:"http://4.236.185.213:8000/api/ingredients-report"
@@ -42,17 +53,86 @@ export default function CostTotals() {
               console.log(error.response.headers);
               }
           });
-    }*/
-
-    if (packaging === undefined) {
-        return (<>loading</>);
     }
+
+    const columns = [
+      { field: 'ingredient_name', headerName: 'Ingredient', width: 120, editable: true },
+      { field: 'meal_name', headerName: 'Meal Name', width: 200 },
+      { field: 'unit_cost', headerName: 'Unit Cost', width: 90, editable: true, valueFormatter: ({ value }) => currencyFormatter.format(value) },
+      { field: 'unit', headerName: 'Measure', width: 90, editable: true },
+//        { field: 'total', headerName: 'Total', width: 90, editable: true },
+      { field: 'qty_on_hand', headerName: 'Qty on Hand', width: 140, type: 'number', editable: false},
+  ]
+
+    function CustomToolbar() {
+      return (
+        <GridToolbarContainer>
+          <GridToolbarExport
+            csvOptions={{
+                fileName: 'Meal Plan Report',
+                delimeter: ';'
+            }} />
+        </GridToolbarContainer>
+      );
+    }
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      getDBMealPlanReport(dateRange);
+    }
+
+    useEffect(() => {
+      getDBIngredients();
+  }, []);
 
     // The HTML structure of this component
     return (
-        <div className='table-div'>
-            {/* Show a row for each entry in packaging.*/}
-            <ReusableTable data={packaging}/>
+        <div>
+          <Typography variant='h5'>Cost Total Report</Typography>
+          <Typography variant='p' sx={{marginBottom: '5%'}}>Select date range to calculate the total costs from start date to end date.</Typography>
+          <form onSubmit={handleSubmit}>
+            {/* <Stack direction='row'> */}
+              <FormControl>
+                <Typography htmlFor="startDate">Start Date: </Typography>
+                <Input id="startDate" variant='outlined' type='date' value={dateRange.startDate} onChange={(event) => {setDateRange({...dateRange, startDate: event.target.value})}}/>
+              </FormControl>
+              <FormControl>
+                <Typography htmlFor="endDate">End Date: </Typography>
+                <Input id="endDate" variant='outlined' type='date' value={dateRange.endDate} onChange={(event) => {setDateRange({...dateRange, endDate: event.target.value})}}/>  
+              </FormControl>  
+            {/* </Stack> */}
+            <FormControl>
+              <Button variant='contained' type='submit'>Submit</Button>
+            </FormControl>
+          </form>
+          <Box sx={{height: '70vh'}}>
+            <DataGrid
+              columns={columns}
+              rows={ingredients}
+              getRowId={(row) => row ? row.i_id : 0}
+              autoHeight={true}
+              components={{Toolbar: CustomToolbar}}
+            >
+            </DataGrid>
+          </Box>
+          <Snackbar
+            open={searchingSBOpen}
+            autoHideDuration={3000}
+            onClose={() => setSearchingSBOpen(false)}
+            message="Searching..."
+          />
+          <Snackbar
+            open={resultsFoundSBOpen}
+            autoHideDuration={3000}
+            onClose={() => setResultsFoundSBOpen(false)}
+            message="Search Complete!"
+          />
+          <Snackbar
+            open={noResultsSBOpen}
+            autoHideDuration={3000}
+            onClose={() => setNoResultsSBOpen(false)}
+            message="No Results Found."
+          />
         </div>
     )
 }
