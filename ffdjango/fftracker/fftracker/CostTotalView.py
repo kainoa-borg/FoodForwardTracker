736d07b5c6ad40f85f5f56c397483d7929510c70
A. McNeese <angela.mcneese@ucdenver.edu>
2023-03-31 16:48:36 -0600

    cost totals subtotals

--- ffdjango/fftracker/fftracker/CostTotalView.py
+++ ffdjango/fftracker/fftracker/CostTotalView.py
@@ -14,22 +14,24 @@ from datetime import date
 import logging
 logging.basicConfig(level = logging.WARNING)
 
-
 class CostTotalSerializer(ModelSerializer):
-	#isupplier = serializers.CharField(read_only=True)
-	#pref_isupplier = serializers.CharField(read_only=True)
-	isupplier = serializers.CharField(max_length=200)
-	pref_isupplier = serializers.CharField(max_length=200)
+	isupplier = SupplierSerializer(read_only=True)
+	psupplier = SupplierSerializer(read_only=True)
+	pref_isupplier = SupplierSerializer(read_only=True)
+	pref_psupplier = SupplierSerializer(read_only=True)
 	class Meta():
-		model = Ingredients
+		model = Ingredients, Packaging
 		fields = ('i_id', 'ingredient_name', 'pkg_type', 'storage_type', 'in_date', 'in_qty', 'qty_on_hand', 'unit', 'exp_date', 'unit_cost', 'flat_fee', 'isupplier_id', 'pref_isupplier_id', 'isupplier', 'pref_isupplier')
-		read_only_fields = ('i_id',)
+                #, ('p_id', 'package_type', 'unit_qty', 'qty_holds', 'unit', 'returnable', 'in_date', 'in_qty', 'qty_on_hand', 'unit_cost', 'flat_fee', 'psupplier_id', 'pref_psupplier_id', 'psupplier', 'pref_psupplier')
+		read_only_fields = ('i_id') #, 'p_id')
 
 class CostTotalView(viewsets.ViewSet):
     def list(self, request):
         startDate = request.query_params.get('startDate')
         endDate = request.query_params.get('endDate')
-        queryset = Ingredients.objects.filter(in_date__range=[startDate, endDate]).order_by('-in_date')
+        querysetIng = Ingredients.objects.filter(in_date__range=[startDate, endDate]).order_by('-in_date')
+        querysetPack = Packaging.objects.filter(in_date__range=[startDate, endDate]).order_by('-in_date')
+        queryset = [querysetIng, querysetPack]
         serializer = CostTotalSerializer(queryset, many=True)
         return Response(serializer.data)
 
