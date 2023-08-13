852b2fb8b6699e5a9ab7532d5f7ae29dd2189ce9
kaiborg <kainoa.accs@gmail.com>
2023-08-13 08:13:03 -1000

    Working commit

--- ffdjango/fftracker/fftracker/MealPlanReportViews.py
+++ ffdjango/fftracker/fftracker/MealPlanReportViews.py
@@ -6,8 +6,9 @@ from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from rest_framework import status
 import datetime
+import sys
 from .models import MealPlans, Recipes, Households, Kits, KitPackaging, RecipePackaging, Packaging, PackagingUsages, HhMealPlans, ServingCalculations
-import json
+from django.http import JsonResponse
 
 # Filters for duplicates, selecting only the most recent items
 def get_latest_items(queryset, fieldName):
@@ -62,129 +63,118 @@ class MealPlanReportView(viewsets.ViewSet):
         queryset = MealPlans.objects.filter(m_date__range=[startDate, endDate]).order_by('m_date')
         serializer = MealPlanReportSerializer(queryset, many=True)
         return Response(serializer.data)
+    
+    # Helper function, handle packaging and usages
+    def handle_packaging(self, package_id_dict, needed_packages, kit, r_packaging, these_servings, total_servings):
+        # Perform operations on these r_packaging
+        for packaging in r_packaging:
+            packaging_options = Packaging.objects.filter(package_type = packaging.pkg_type).order_by('-qty_on_hand')
+            # Total_qty available for this package type
+            total_qty = 0
+            # Calculate total_qty across all options, add each to dict
+            for pkg in packaging_options:
+                total_qty += pkg.qty_on_hand
+                if pkg.p_id not in package_id_dict:
+                    package_id_dict[pkg.p_id] = {'qty_on_hand': pkg.qty_on_hand, 'used_qty': 0}
+            # If there isn't enough packaging for this meal, add it to the needed packaging list
+            if (total_qty < total_servings):
+                if packaging.pkg_type not in needed_packages:
+                    needed_packages[packaging.pkg_type] = {'package_type': packaging.pkg_type, 'qty_needed': total_servings - total_qty}
+                else:
+                    needed_packages[packaging.pkg_type]['qty_needed'] += total_servings - total_qty
+            # If there is enough packaging for this meal, we can calculate usages and add it to kits
+            else:
+                # Calculate usages across each packaging option
+                for pkg in packaging_options:
+                    # If this entry has enough for this houshold
+                    if package_id_dict[pkg.p_id]['qty_on_hand'] >= these_servings:
+                        # Take from this inventory entry
+                        if (KitPackaging.objects.count() > 0):
+                            latest_key = KitPackaging.objects.latest('kp_id').kp_id + 1
+                        else:
+                            latest_key = 0
+                        KitPackaging.objects.create(kp_id=latest_key, kp_p_id=pkg.p_id, qty=these_servings, kp_kit=kit)
+                        package_id_dict[pkg.p_id]['used_qty'] += these_servings
+                        package_id_dict[pkg.p_id]['qty_on_hand'] -= these_servings
+                        these_servings = 0
+                    # If this entry doesn't have enough for all of this household
+                    elif package_id_dict[pkg.p_id]['qty_on_hand'] > 0:
+                        # Use what is left and continue
+                        if (KitPackaging.objects.count() > 0):
+                            latest_key = KitPackaging.objects.latest('kp_id').kp_id + 1
+                        else:
+                            latest_key = 0
+                        KitPackaging.objects.create(kp_id=latest_key, kp_p_id=pkg.p_id, qty=pkg.qty_on_hand, kp_kit=kit)
+                        package_id_dict[pkg.p_id]['used_qty'] += these_servings
+                        package_id_dict[pkg.p_id]['qty_on_hand'] -= these_servings
+                        these_servings -= pkg.qty_on_hand
+                    # If we have packages for all servings, go to next package type
+                    if these_servings <= 0:
+                        break
+
+
     # Get this meal plan and calculate servings for it
     def retrieve(self, request, pk):
+        # Get the meal plan to perform calculations with
         meal_plan = MealPlans.objects.get(m_id=pk)
-        queryset = Households.objects.filter(paused_flag=False)
+        # Get the households to calculate this meal plan for (not paused)
+        households = Households.objects.filter(paused_flag=False)
+        # packaging needed for this meal and snack
         meal_r_packaging = RecipePackaging.objects.filter(rp_recipe_num = meal_plan.meal_r_num)
         snack_r_packaging = RecipePackaging.objects.filter(rp_recipe_num = meal_plan.snack_r_num)
         meal_servings = 0
         snack_servings = 0
 
+        # Dict to calculate total usages across available packaging
         package_id_dict = dict()
+        # List of packaging that needs more inventory
+        needed_packaging = dict()
 
         serving_calculation = None
+        # If a serving calculation already exists, delete it and create a new one
         try:
             serving_calculation = ServingCalculations.objects.get(calc_meal_plan=meal_plan)
             serving_calculation.delete()
             serving_calculation = ServingCalculations.objects.create(calc_meal_plan=meal_plan, calc_date=datetime.date.today())
+        # Otherwise, just create a new one
         except:
             serving_calculation = ServingCalculations.objects.create(calc_meal_plan=meal_plan, calc_date=datetime.date.today())
 
+        # Attempt calculations
         try:
-            for household in queryset:
+            for household in households:
                 # Calculate meal servings for this household
                 hh_meal_servings = household.num_adult + household.num_child_gt_6 + (household.num_child_lt_6 *.5)
                 hh_snack_servings = household.num_adult + household.num_child_gt_6 + household.num_child_lt_6
                 meal_servings += hh_meal_servings
                 snack_servings += hh_snack_servings
 
-                # Only create kits for this planned meal if it requires packaging
-                if (meal_r_packaging.count() > 0 or snack_r_packaging.count() > 0):
-                    if (Kits.objects.count() > 0):
-                        latest_kit_key = Kits.objects.latest('k_id').k_id + 1
-                    else:
-                        latest_kit_key = 0
-                    kit = Kits.objects.create(k_date=meal_plan.m_date, k_hh_id=household, k_s_c=serving_calculation)
-                    if (HhMealPlans.objects.count() > 0):
-                        latest_hhmp_key = HhMealPlans.objects.latest('hh_m_id').hh_m_id + 1
-                    else:
-                        latest_hhmp_key = 0
-                    HhMealPlans.objects.create(meal_id=meal_plan.m_id, meal_hh_id=household, hh_s_c=serving_calculation)
+                # Create the kit and meal plan for this household
+                if (Kits.objects.count() > 0):
+                    latest_kit_key = Kits.objects.latest('k_id').k_id + 1
+                else:
+                    latest_kit_key = 0
+                kit = Kits.objects.create(k_date=meal_plan.m_date, k_hh_id=household, k_s_c=serving_calculation)
+                if (HhMealPlans.objects.count() > 0):
+                    latest_hhmp_key = HhMealPlans.objects.latest('hh_m_id').hh_m_id + 1
+                else:
+                    latest_hhmp_key = 0
+                HhMealPlans.objects.create(meal_id=meal_plan.m_id, meal_hh_id=household, hh_s_c=serving_calculation)
 
-                # Perform operations on meal packaging
-                for packaging in meal_r_packaging:
-                    packaging_options = Packaging.objects.filter(package_type = packaging.pkg_type).order_by('-qty_on_hand')
-                    # Total_qty available for this package type
-                    total_qty = 0
-                    # Calculate total_qty across all options, add each to dict
-                    for pkg in packaging_options:
-                        total_qty += pkg.qty_on_hand
-                        if pkg.p_id not in package_id_dict:
-                            package_id_dict[pkg.p_id] = {'qty_on_hand': pkg.qty_on_hand, 'used_qty': 0}
-                    # If there isn't enough packaging for this meal, return an error
-                    if (total_qty < meal_servings):
-                        # Calculate servings failed, delete all created objects and return error message
-                        serving_calculation.delete()
-                        meal_plan.meal_servings=None
-                        meal_plan.snack_servings=None
-                        meal_plan.save()
-                        return Response({"errorText": "Not enough %ss in inventory"%(packaging.pkg_type)}, 400)
-                    # Calculate usages across each packaging option
-                    for pkg in packaging_options:
-                        # Get latest key for packaging usages        
+                # Calculate usages and create kits across meal and snack recipe packaging
+                self.handle_packaging(package_id_dict, needed_packaging, kit, meal_r_packaging, meal_servings, hh_meal_servings)
+                self.handle_packaging(package_id_dict, needed_packaging, kit, snack_r_packaging, snack_servings, hh_snack_servings)
 
-                        # If this entry has enough for this houshold
-                        if package_id_dict[pkg.p_id]['qty_on_hand'] >= hh_meal_servings:
-                            # Take from this inventory entry
-                            if (KitPackaging.objects.count() > 0):
-                                latest_key = KitPackaging.objects.latest('kp_id').kp_id + 1
-                            else:
-                                latest_key = 0
-                            KitPackaging.objects.create(kp_id=latest_key, kp_p_id=pkg.p_id, qty=hh_meal_servings, kp_kit=kit)
-                            package_id_dict[pkg.p_id]['used_qty'] += hh_meal_servings
-                            package_id_dict[pkg.p_id]['qty_on_hand'] -= hh_meal_servings
-                            hh_meal_servings = 0
-                        # If this entry doesn't have enough for all of this household
-                        elif package_id_dict[pkg.p_id]['qty_on_hand'] > 0:
-                            # Use what is left and continue
-                            if (KitPackaging.objects.count() > 0):
-                                latest_key = KitPackaging.objects.latest('kp_id').kp_id + 1
-                            else:
-                                latest_key = 0
-                            KitPackaging.objects.create(kp_id=latest_key, kp_p_id=pkg.p_id, qty=pkg.qty_on_hand, kp_kit=kit)
-                            package_id_dict[pkg.p_id]['used_qty'] += hh_meal_servings
-                            package_id_dict[pkg.p_id]['qty_on_hand'] -= hh_meal_servings
-                            hh_meal_servings -= pkg.qty_on_hand
-                        # If we have packages for all servings, go to next package type
-                        if hh_meal_servings <= 0:
-                            break
-
-                # Perform operations on snack_packaging
-                for packaging in snack_r_packaging:
-                    packaging_options = Packaging.objects.filter(package_type = packaging.pkg_type).order_by('-qty_on_hand')
-                    total_qty = 0
-                    for pkg in packaging_options:
-                        total_qty += pkg.qty_on_hand
-                        if pkg.p_id not in package_id_dict:
-                            package_id_dict[pkg.p_id] = {'qty_on_hand': pkg.qty_on_hand, 'used_qty': 0}
-                    if (total_qty < snack_servings):
-                        # Return not enough quantity on hand error message !!!
-                        return Response("Not enough %ss in inventory"%(packaging.pkg_type), 400)
-                    for pkg in packaging_options:
-
-                        if package_id_dict[pkg.p_id]['qty_on_hand'] >= hh_snack_servings:
-                            if (KitPackaging.objects.count() > 0):
-                                latest_key = KitPackaging.objects.latest('kp_id').kp_id + 1
-                            else:
-                                latest_key = 0
-                            KitPackaging.objects.create(kp_id=latest_key, kp_p_id=pkg.p_id, qty=hh_snack_servings, kp_kit=kit)
-                            package_id_dict[pkg.p_id]['used_qty'] += hh_snack_servings
-                            package_id_dict[pkg.p_id]['qty_on_hand'] -= hh_snack_servings
-                            hh_snack_servings = 0
-                        elif package_id_dict[pkg.p_id]['qty_on_hand'] > 0:
-                            if (KitPackaging.objects.count() > 0):
-                                latest_key = KitPackaging.objects.latest('kp_id').kp_id + 1
-                            else:
-                                latest_key = 0
-                            KitPackaging.objects.create(kp_id=latest_key, kp_p_id=pkg.p_id, qty=pkg.qty_on_hand, kp_kit=kit)
-                            package_id_dict[pkg.p_id]['used_qty'] += hh_snack_servings
-                            package_id_dict[pkg.p_id]['qty_on_hand'] -= hh_snack_servings
-                            hh_snack_servings -= pkg.qty_on_hand
-                        if hh_snack_servings <= 0:
-                            break
+            if len(needed_packaging) > 1:
+                # Delete this serving calculation, we cannot prepare the meal
+                serving_calculation.delete()
+                return Response({'needed_packaging': needed_packaging.values()}, 200)
+                # return JsonResponse(needed_packaging, safe=False)
+        
+        # Error during calculation, delete the attempt and return error.
         except:
             print("Exception Here")
+            print(sys.exc_info())
             # Calculate servings failed, clear package ids, delete this serving calc run, and return error message
             package_id_dict = {}
             serving_calculation.delete()
@@ -192,8 +182,6 @@ class MealPlanReportView(viewsets.ViewSet):
             meal_plan.snack_servings=None
             meal_plan.save()
             return Response({"errorText": "Internal Server Error"}, 500)
-
-
         
         # Create usages on these packaging entries
         for pkg_id in package_id_dict.keys():
